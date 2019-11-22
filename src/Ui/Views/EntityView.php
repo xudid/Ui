5502deff4a5e7c505b24dfe6ed5960b69a989a54
Didier Moindreau <Didux74@gmail.com>
2019-11-22 14:32:13 +0100

    Review Page and EntityView

--- src/Ui/Views/EntityView.php
+++ src/Ui/Views/EntityView.php
@@ -2,7 +2,6 @@
 namespace Ui\Views;
 use Ui\HTML\Elements\Nested\Section;
 use Ui\HTML\Elements\Nested\P;
-use Ui\HTML\Elements\Empties\Br;
 
 /**
  * Class EntityView
@@ -11,8 +10,8 @@ use Ui\HTML\Elements\Empties\Br;
  */
 class EntityView extends Section {
 
-    private $title = "";
-    private $name = "";
+    private string $title = "";
+    private string $name = "";
     private $titleElement=null;
 
     /**
@@ -20,27 +19,34 @@ class EntityView extends Section {
      */
     public function __construct() {
         parent::__construct();
-        $this->childElements = array();
+        $this->childs = [];
     }
 
     /**
-     * @param $title
+     * @param string $title
+     * @return $this
      */
-    public function setTitle($title) {
+    public function setTitle(string $title) {
         $this->title = $title;
         $this->titleElement = (new p($this->title))->add($this->title);
         $this->titleElement->setClass("form_title");
         $this->setFirstElement($this->titleElement);
-
+        return $this;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name) {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function __toString():string {
         return parent::__toString();
     }
 }
-
--- src/Ui/Views/Page.php
+++ src/Ui/Views/Page.php
@@ -1,113 +1,110 @@
 <?php
+
 namespace Ui\Views;
 
-use Ui\HTML\Elements\Nested\{
-		Nested,
-		Script,
-		Html,
-		Body,
-		Head
-	};
-
 use Ui\HTML\Attributes\GlobalAttribute;
-use Ui\HTML\Elements\Empties\Link;
+use Ui\HTML\Elements\Nested\{Body, Head, Html, Nested, Script};
+
 /**
  * Class Page
  * @package Ui\Views
  */
-class Page extends Nested{
+class Page extends Nested
+{
 
-	protected $doctype="html";
-	private $htmle = null;
-	private $head = null;
-	private $body = null;
-	/**
-	 * @var array
-	 */
-	private array $scriptToEnd = [];
-
-
-	private function renderDoctype()
-	{
-
-		return "<!DOCTYPE ".$this->doctype.'>';
-	}
+    protected string $doctype = "html";
+    private $htmle = null;
+    private $head = null;
+    private $body = null;
+    /**
+     * @var array
+     */
+    private array $scriptToEnd = [];
 
 
-	/**
-	 * Page constructor.
-	 */
-	public function __construct(){
+    private function renderDoctype()
+    {
+        return "<!DOCTYPE " . $this->doctype . '>';
+    }
 
 
-			$this->htmle = new Html();
+    /**
+     * Page constructor.
+     */
+    public function __construct()
+    {
+	    parent::__construct();
+        $this->htmle = new Html();
+        $this->head = new Head();
+        $this->body = new Body();
+        $this->htmle->add($this->head);
+        $this->htmle->add($this->body);
+        return $this;
+    }
 
-			$this->head = new Head();
-			$this->body = new Body();
+    /**
+     * @param $base
+     * @return $this
+     */
+    public function setBase(string $base)
+    {
+        $this->head->setBase($base);
+        return $this;
+    }
 
-			$this->htmle->add($this->head);
-			$this->htmle->add($this->body);
-			return $this;
-	}
+    /**
+     * @param $lang
+     * @return $this
+     */
+    public function setLang(string $lang)
+    {
+        $this->htmle->setAttribute(GlobalAttribute::LANG, $lang);
+        return $this;
+    }
 
-	/**
-	 * @param $base
-	 * @return $this
-	 */
-	public function setBase(string $base){
-		$this->head->setBase($base);
-		return $this;
-	}
+    /**
+     * @param string $title
+     * @return self
+     */
+    public function setTitle(string $title)
+    {
+        $this->head->setTitle($title);
+        return $this;
+    }
 
-	/**
-	 * @param $lang
-	 * @return $this
-	 */
-	public function setLang(string $lang){
-			$this->htmle->setAttribute(GlobalAttribute::LANG,$lang);
-			return $this;
-	}
+    /**
+     * @param mixed $element
+     * @return self
+     */
+    public function feedBody(...$elements)
+    {
+        $this->body->feed($elements);
+        return $this;
+    }
 
-	/**
-	 * @param string $title
-	 * @return self
-	 */
-	public function setTitle(string $title){
-			$this->head->setTitle($title);
-			return $this;
-	}
+    /**
+     * @param $css
+     */
+    public function addBodyCss($css)
+    {
+        $this->body->setClass($css);
+    }
 
-	/**
-	 * @param mixed$element
-	 * @return self
-	 */
-	public function addToBody($element){
-			$this->body->add($element);
-			return $this;
-	}
-
-	/**
-	 * @param $css
-	 */
-	public function addBodyCss($css)
-	{
-		$this->body->setClass($css);
-	}
-
-	/**
-	 * @param Script $script
-	 * @return void
-	 */
-	public function addScript(Script $script) {
-
-		$position = $script->getPosition();
-		if ($script instanceof Script && strcmp($position, Script::SCRIPT_TO_END)==0) {
-			$this->scriptToEnd[] = $script;
-
-		} elseif ($script instanceof Script && strcmp($position, Script::SCRIPT_TO_HEAD)==0) {
-			$this->head->addScript($script);
-		}
-	}
+    /**
+     * @param Script $script
+     * @return void
+     */
+    public function addScript(Script $script)
+    {
+        $position = $script->getPosition();
+        if ($script instanceof Script &&
+            strcmp($position, Script::SCRIPT_TO_END) == 0) {
+                $this->scriptToEnd[] = $script;
+        } elseif ($script instanceof Script
+            && strcmp($position, Script::SCRIPT_TO_HEAD) == 0) {
+                $this->head->addScript($script);
+        }
+    }
 
 
     /**
@@ -115,71 +112,66 @@ class Page extends Nested{
      * @param string $css : the css file path or URI
      * @return self
      */
-	public function importCss(...$cssPaths)
-	{
+    public function importCss(...$cssPaths)
+    {
+        foreach ($cssPaths as $cssPath) {
+            $this->head->addLink($cssPath, "stylesheet");
+        }
+        return $this;
+    }
 
-		foreach ($cssPaths as $cssPath) {
-			$this->head->addLink($cssPath,"stylesheet");
-		}
-		return $this;
+    public function importScript(...$scripts)
+    {
+        foreach ($scripts as $script) {
+            $this->addScript($script);
+        }
+        return $this;
+    }
 
-	}
+    /**
+     * @param $meta
+     * @return $this
+     */
+    public function addMeta($meta)
+    {
+        $this->head->addMeta($meta);
+        return $this;
+    }
 
-	public function importScript(...$scripts)
-	{
-		foreach ($scripts as $script) {
-			$this->addScript($script);
-		}
-		return $this;
-	}
+    public function importMeta(...$metas)
+    {
+        foreach ($metas as $meta) {
+            $this->addMeta($meta);
+        }
+    }
 
-	/**
-	 * @param $meta
-	 * @return $this
-	 */
-	public function addMeta($meta){
-		$this->head->addMeta($meta);
-		return $this;
-	}
-
-	public function importMeta(...$metas)
-	{
-		foreach ($metas as $meta) {
-			$this->addMeta($meta);
-		}
-	}
-
-	/**
-	 * @return string
-	 */
-	public function __toString():string{
-		foreach ($this->scriptToEnd as $script) {
-			$this->addToBody($script);
-		}
-		$string ="";
-		$string = $string.$this->renderDoctype();
-		$string = $string.$this->htmle->__toString() ;
-		return $string;
-	}
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        foreach ($this->scriptToEnd as $script) {
+            $this->addToBody($script);
+        }
+        $string = "";
+        $string = $string . $this->renderDoctype();
+        $string = $string . $this->htmle->__toString();
+        return $string;
+    }
 
     /**
      * @Deprecated use __toString() instead
      */
-	public function render(){
-		$this->renderDoctype();
-
-
-        $string ="";
-
-        $string = $string.$this->renderDoctype();
-        foreach ($this->childElements as $value) {
-                $string =$string.$value."\r\n";
-
-
+    public function render()
+    {
+        $this->renderDoctype();
+        $string = "";
+        $string = $string . $this->renderDoctype();
+        foreach ($this->childs as $value) {
+            $string = $string . $value . "\r\n";
         }
         $this->contentString = $string;
-
-	$this->renderBody($this->contentString);
-	$this->renderHtmlCloseBalise();
-	}
+        $this->renderBody($this->contentString);
+        $this->renderHtmlCloseBalise();
+    }
 }
