3881cd9f6ed6a051b4170ed415f48d7d92342902
Didier Moindreau <dmoindreau@gmail.com>
2021-01-26 23:38:58 +0100

    BString type hinting

--- src/Ui/Utils/BString.php
+++ src/Ui/Utils/BString.php
@@ -1,37 +1,38 @@
 <?php
-namespace Brick\Utils;
+
+namespace Ui\Utils;
 
 /**
  * BString class offers utils function on string
  */
 class BString
 {
-  /**
-  * @param string $string :a string
-  * @param string $start : the string start to test if exists
-  */
-    public static function startsWith($string, $start)
-    {
-        $res = false;
-        if (strpos($string, $start) === 0) {
-            $res = true;
-        }
-        return $res;
-    }
-  /**
-   * @param string $string :a string
-   * @param string $end : the string end to test if exists
-   */
-    public static function endsWith($string, $end)
-    {
+	/**
+	 * @param string $string :a string
+	 * @param string $start : the string start to test if exists
+	 * @return bool
+	 */
+	public static function startsWith(string $string, string $start): bool
+	{
+		$res = false;
+		if (strpos($string, $start) === 0) {
+			$res = true;
+		}
+		return $res;
+	}
 
-
-
-          $res = false;
-        if (((strrpos($string, $end) + strlen($end)) ===
-             strlen($string))) {
-            $res = true;
-        }
-            return $res;
-    }
+	/**
+	 * @param string $string :a string
+	 * @param string $end : the string end to test if exists
+	 * @return bool
+	 */
+	public static function endsWith(string $string, string $end): bool
+	{
+		$res = false;
+		if (((strrpos($string, $end) + strlen($end)) ===
+			strlen($string))) {
+			$res = true;
+		}
+		return $res;
+	}
 }
